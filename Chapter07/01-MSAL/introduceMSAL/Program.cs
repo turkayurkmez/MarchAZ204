640e6b22ab0bf5e05d9dc6ba441d65842bcc576b
Türkay Ürkmez <turkay@turkayurkmez.com>
2023-03-02 20:06:07 +0300

    lastchanges

---
+++ Chapter07/01-MSAL/introduceMSAL/Program.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.Identity.Client;
+// See https://aka.ms/new-console-template for more information
+const string _clientId ="62ef295f-fe61-4690-9afb-9eceb5f0740d";
+const string _tenantId ="714700c6-10e2-4ca4-b315-06d7fe91ee94";
+
+var app = PublicClientApplicationBuilder
+    .Create(_clientId)
+    .WithAuthority(AzureCloudInstance.AzurePublic, _tenantId)
+    .WithRedirectUri("http://localhost")
+    .Build();
+
+string[] scopes = { "User.Read","Calendars.Read" };
+
+AuthenticationResult result = await app.AcquireTokenInteractive(scopes).ExecuteAsync();
+
+Console.WriteLine($"ID:\n{result.IdToken}");
+Console.WriteLine($"Access:\n{result.AccessToken}");
+
---
+++ Chapter08/01-getSecret/HttpTrigger1.cs
@@ -0,0 +1,48 @@
+using System;
+using System.IO;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+using Azure.Identity;
+using Azure.Security.KeyVault.Secrets;
+
+namespace Company.Function
+{
+    public static class HttpTrigger1
+    {
+        [FunctionName("HttpTrigger1")]
+        public static async Task<IActionResult> Run(
+            [HttpTrigger(AuthorizationLevel.Anonymous, "get", "post", Route = null)] HttpRequest req,
+            ILogger log)
+        {
+            log.LogInformation("C# HTTP trigger function processed a request.");
+
+            string userAssignedClientId = "c71034a0-0914-4600-bcc8-028145de895c";
+            var credential = new DefaultAzureCredential(new DefaultAzureCredentialOptions{ 
+                ManagedIdentityClientId = userAssignedClientId 
+            });
+
+            var client = new SecretClient(new Uri("https://bademo.vault.azure.net/"),credential);
+
+            var secret = await client.GetSecretAsync("secretTest");
+
+
+
+            // string name = req.Query["name"];
+
+            // string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
+            // dynamic data = JsonConvert.DeserializeObject(requestBody);
+            // name = name ?? data?.name;
+
+            // string responseMessage = string.IsNullOrEmpty(name)
+            //     ? "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response."
+            //     : $"Hello, {name}. This HTTP triggered function executed successfully.";
+
+            return new OkObjectResult($"Gizli değerimiz: {secret.Value.Value}");
+        }
+    }
+}
